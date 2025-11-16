1a04af5b1dfb4d6a1b1ff98f77bf4f44a68d6eec
farzin mossaed <fmossaed@gmail.com>
2025-11-16 14:51:28 +0330

    work with react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-
 import './App.css'
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import Root from "./routes/Root.jsx";
@@ -7,17 +6,40 @@ import Welcome from "./routes/Welcome.jsx";
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <Root />,
+    element: <Root/>,
     children: [
-      {index: true, element: <Welcome />},
+      {index: true, element: <Welcome/>},
       {
         path: '/products',
-        loader: async () => {
+        loader: async (args) => {
+          console.log(args)
           await new Promise(r => setTimeout(r, 1500)); // simulate API delay
-          return { message: "This is Products Page" };
+          return {message: "This is Products Page"};
         },
-        lazy: () => import('./routes/Products.jsx')},
-      {path: '/products/:id', lazy: () => import('./routes/ProductsDetail.jsx')}
+        lazy: () => import('./routes/Products.jsx')
+      },
+      {
+        path: '/products/:id',
+        loader: async (args) => {
+          console.log(args)
+          // await new Promise(r => setTimeout(r, 1500)); // simulate API delay
+          // return { message: `This is Product ${args.params.id} Detail Page` };
+          const response = await fetch(
+              'https://jsonplaceholder.typicode.com/posts/' + args.params.id
+          );
+
+          let result = await response.json()
+          console.log(result)
+
+          if(!response.ok)
+          {
+            throw new Error('Could not fetch post for id ' + args.params.id);
+          }
+
+          return result;
+        },
+        lazy: () => import('./routes/ProductsDetail.jsx')
+      }
     ]
   }
 ])
@@ -25,9 +47,9 @@ const router = createBrowserRouter([
 function App() {
 
   return (
-    <>
-      <RouterProvider router={router} />
-    </>
+      <>
+        <RouterProvider router={router}/>
+      </>
   )
 }
 
--- src/routes/ProductsDetail.jsx
+++ src/routes/ProductsDetail.jsx
@@ -1,11 +1,16 @@
-import {useParams} from "react-router-dom";
+import {useLoaderData, useParams} from "react-router-dom";
 
 const ProductsDetail = () => {
   const params = useParams()
+  const data = useLoaderData();
   return (
-      <>
+      <div style={{display: 'flex', alignItems: 'center', flexDirection: 'column'}}>
         <span>products detail: {params.id}</span>
-      </>
+        <span>id: {data.id}</span>
+        <span>userId: {data.userId}</span>
+        <span>title: {data.title}</span>
+        <span>body: {data.body}</span>
+      </div>
   )
 }
 
