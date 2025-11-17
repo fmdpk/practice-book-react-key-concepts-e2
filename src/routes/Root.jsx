3bf86988a047d4a18eb2bd59989238f1b0595ce7
farzin mossaed <fmossaed@gmail.com>
2025-11-17 10:50:14 +0330

    add animation to route transition

--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -1,5 +1,6 @@
 import MainNavigation from "../components/MainNavigation.jsx";
 import {Outlet, useNavigation} from "react-router-dom";
+import {AnimatePresence, motion} from "framer-motion";
 
 const Root = () => {
   const navigation = useNavigation()
@@ -7,7 +8,19 @@ const Root = () => {
       <>
         {navigation.state === 'loading' && <span>loading ...</span>}
         <MainNavigation/>
-        <Outlet/>
+        {/*<Outlet/>*/}
+        <AnimatePresence mode="wait">
+          <motion.div
+              key={location.pathname}
+              initial={{ scale: 0.9, opacity: 0 }}
+              animate={{ scale: 1, opacity: 1 }}
+              // exit={{ scale: 0.9, opacity: 0 }}
+              transition={{ duration: 0.3, ease: "easeInOut" }}
+              style={{ position: "relative" }}
+          >
+            <Outlet />
+          </motion.div>
+        </AnimatePresence>
       </>
   )
 }
